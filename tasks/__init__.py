dfd4a3e5c9510567e948c17eab7927e227c50717
Mykola <nchernov@gmail.com>
2023-05-11 16:34:30 +0300

    create superuser

--- tasks/__init__.py
+++ tasks/__init__.py
@@ -1,4 +1,9 @@
 # flake8: noqa F401
+import os
+import sys
+
+sys.path += [os.curdir]
+
 from .shell import shell
 
 from .superuser import create_superuser
--- tasks/superuser.py
+++ tasks/superuser.py
@@ -1,15 +1,16 @@
 from invoke import task
 
+from app.logger import log
+from app import get_db, get_settings, make_hash
+from app import schema as s
+
+db = next(get_db())
+cfg = get_settings()
+
 
 @task
 def create_superuser(_):
     """Adds admin(default) user"""
-    from app.logger import log
-    from app import get_db, get_settings, make_hash
-    from app import schema as s
-
-    db = next(get_db())
-    cfg = get_settings()
 
     if db["user"].find_one({"email": cfg.ADMIN_EMAIL}):
         log(log.WARNING, "SuperUser -%s already exists", cfg.ADMIN_EMAIL)
