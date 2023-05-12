70afb4196997e6da0551f822fa2997ba4a976ce5
denys <denysburimov@gmai.com>
2023-05-12 15:55:50 +0300

    fix pydantic

--- app/schema/user.py
+++ app/schema/user.py
@@ -15,24 +15,18 @@ class UserLogin(BaseModel):
     password: str
 
 
-class UserOutput(BaseModel):
-    id: str
-    username: str
-    email: EmailStr
-    password_hash: str
-
-
 class UserUpdate(BaseModel):
     username: str
     email: EmailStr
     password_hash: str
 
 
-class UserDB(BaseModel):
+class UserOutput(UserUpdate):
+    id: str
+
+
+class UserDB(UserUpdate):
     id: PyObjectId | None = Field(default_factory=PyObjectId, alias="_id")
-    username: str
-    email: EmailStr
-    password_hash: str
 
     class Config:
         allow_population_by_field_name = True
