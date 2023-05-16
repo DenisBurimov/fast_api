c5d349cd636ae7a2b86e73e87cf1c4541ceb3b72
denys <denysburimov@gmai.com>
2023-05-16 16:19:17 +0300

    update pydantic validation

--- app/schema/sleep.py
+++ app/schema/sleep.py
@@ -14,6 +14,48 @@ class RecordsType(str, enum.Enum):
     PedometerRecord = "pedometer"
 
 
+class DiagnosticRecord(BaseModel):
+    date: str
+    message: str
+    data: dict
+
+
+class SleepRecord(BaseModel):
+    value: int
+    startDate: str
+    endDate: str
+    source: str
+
+
+class AccelerometerRecord(BaseModel):
+    date: str
+    x: float
+    y: float
+    z: float
+
+
+class StepRecord(BaseModel):
+    date: str
+    unknown: int
+    stationary: int
+    walking: int
+    running: int
+    automotive: int
+    cycling: int
+
+
+class PedometerRecord(BaseModel):
+    startDate: str
+    endDate: str
+    numberOfSteps: int
+    distance: float
+    floorsAscended: int
+    floorsDescended: int
+    currentPace: float
+    currentCadence: float
+    averageActivePace: float
+
+
 class SleepData(BaseModel):
     data: dict[str, Union[str, dict, int]]
     type: RecordsType
