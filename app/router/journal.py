14a4ec80f62678e19b82e016e042f9be74fda14e
denys <denysburimov@gmai.com>
2023-05-29 15:52:55 +0300

    user logs added

--- app/router/journal.py
+++ app/router/journal.py
@@ -18,7 +18,6 @@ def add_journal_item(
     db: Database = Depends(get_db),
     _: s.UserDB = Depends(get_current_user),
 ):
-    # res: results.InsertOneResult = db.journal_items.insert_one(data.dict())
     res: results.InsertOneResult = db.journal_items.insert_one(
         {
             "_id": ObjectId(data.id),
@@ -28,7 +27,9 @@ def add_journal_item(
         }
     )
 
-    log(log.INFO, "Journal item [%s] has been saved", res.inserted_id)
+    if res:
+        log(log.INFO, "Journal item [%s] has been saved", res.inserted_id)
+    log(log.INFO, "Something went wrong. Journal item has not been saved")
     return s.JournalDB.parse_obj(db.journal_items.find_one({"_id": res.inserted_id}))
 
 
@@ -62,7 +63,7 @@ def get_journal_item_by_id(
 
 
 @journal_router.delete("/{id}", response_model=s.DeleteMessage)
-def get_delete_user(
+def get_delete_journal_id(
     id: str,
     db: Database = Depends(get_db),
     _: s.UserDB = Depends(get_current_user),
@@ -71,5 +72,6 @@ def get_delete_user(
     if not res.deleted_count:
         raise HTTPException(status_code=404, detail="This user was not found")
 
+    log(log.INFO, "JournalDB [%s] was successfully deleted", id)
     message = f"JournalDB {id} was successfully deleted"
     return s.DeleteMessage(message=message)
--- app/router/user.py
+++ app/router/user.py
@@ -3,8 +3,8 @@ from pymongo.database import Database
 from pymongo import results
 from bson.objectid import ObjectId
 from app import get_db
+from app.logger import log
 import app.schema as s
-
 from app.dependency import get_current_user
 
 user_router = APIRouter(prefix="/user", tags=["User"])
@@ -23,7 +23,11 @@ def get_users(
     Returns:
         schema.UserList class with users field, which is a list of UserDB classes.
     """
-    return s.UserList(users=[s.UserDB.parse_obj(o) for o in db.users.find()])
+    users_list = s.UserList(users=[s.UserDB.parse_obj(o) for o in db.users.find()])
+    if not users_list.users:
+        log(log.ERROR, "Users not found")
+    log(log.INFO, "[%d] users have been fetched", len(users_list.users))
+    return users_list
 
 
 @user_router.get("/{id}", response_model=s.UserDB)
@@ -42,12 +46,14 @@ def get_user_by_id(
         HTTPException: in case if user with given id was not found.
 
     Returns:
-        schema.UserDB parsed by pydantic from the mongo object.
+        schema.UserDB with a given id, parsed by pydantic from the mongo object.
     """
     user = db.users.find_one({"_id": ObjectId(id)})
     if not user:
+        log(log.ERROR, "User with id [%s] was not found", id)
         raise HTTPException(status_code=404, detail="This user was not found")
 
+    log(log.INFO, "User [%s]: %s", id, user)
     return s.UserDB.parse_obj(user)
 
 
@@ -58,8 +64,13 @@ def get_update_user(
     db: Database = Depends(get_db),
     _: s.UserDB = Depends(get_current_user),
 ):
-    db.users.update_one({"_id": ObjectId(id)}, {"$set": data.dict(exclude_none=True)})
+    user_update = db.users.update_one(
+        {"_id": ObjectId(id)}, {"$set": data.dict(exclude_none=True)}
+    )
+    if not user_update.acknowledged:
+        log(log.ERROR, "Something went wrong. Cannot update user [%s]", id)
 
+    log(log.INFO, "User [%s] has been successfully updated", id)
     return s.UserDB.parse_obj(db.users.find_one({"_id": ObjectId(id)}))
 
 
@@ -71,7 +82,9 @@ def get_delete_user(
 ):
     res: results.DeleteResult = db.users.delete_one({"_id": ObjectId(id)})
     if not res.deleted_count:
+        log(log.ERROR, "User [%s] was not found", id)
         raise HTTPException(status_code=404, detail="This user was not found")
 
+    log(log.INFO, "User [%s] was successfully deleted", id)
     message = f"User {id} was successfully deleted"
     return s.DeleteMessage(message=message)
