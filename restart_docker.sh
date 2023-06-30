91d4acbaf70256be32647a46341eb9f9a3eb01b1
denys <denysburimov@gmai.com>
2023-06-30 11:54:03 +0300

    restart docker script

---
+++ restart_docker.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+git pull
+docker-compose build
+if [ $? -eq 0 ]
+then
+    docker-compose down
+    docker-compose up -d
+    if [ $? -eq 0]
+    then
+        echo "Containers have been started successfully"
+    else
+        echo "Exit code: " $?
+else
+    echo "Build failed"
